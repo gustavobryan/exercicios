4c215ab3395e09aa03578b331dd5933d7c264462
Gustavo Bryan <devgustavob@gmail.com>
2023-05-08 17:28:38 -0300

    commit inicial

---
+++ src/exerciciosp2/Main.java
@@ -0,0 +1,25 @@
+package exerciciosp2;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double nota1 = sc.nextDouble();
+		double nota2 = sc.nextDouble();
+		
+		double notaFinal = nota1 + nota2;
+		
+		System.out.printf("Nota final = %.1f%n", notaFinal);
+		
+		if (notaFinal < 60.0) {
+			System.out.println("Reprovado");
+		}
+		
+		sc.close();
+	}
+}
