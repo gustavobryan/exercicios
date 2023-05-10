4402153a782b1a83383d2ce650be35c667bafca3
Gustavo Bryan <devgustavob@gmail.com>
2023-05-09 21:00:53 -0300

    multiplos

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,19 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-	
-	// bhaskara
-	
+
 	public static void main(String[] args) {
-		
-		Locale.setDefault(Locale.US);
+
 		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
 		
-		double a = sc.nextDouble();
-		double b = sc.nextDouble();
-		double c = sc.nextDouble();
-		
-		double delta = b * b -4.0 * a *c;
-		
-		if (a == 0 || delta < 0.0) {
-			System.out.println("Impossivel calcular");
+		if (A % B == 0 || B % A == 0) {
+			System.out.println("Sao Multiplos");
 		}
 		else {
-			double r1 = (-b + Math.sqrt(delta)) / (2.0 * a);
-			double r2 = (-b - Math.sqrt(delta)) / (2.0 * a);
-			System.out.printf("R1 = %.5f%n", r1);
-			System.out.printf("R2 = %.5f%n", r2);
-
-
+			System.out.println("Nao sao Multiplos");
 		}
 		
 		sc.close();
